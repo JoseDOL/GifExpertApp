86f46bf4d189a428e66da373b9cfceb275683e05
JoseDOL <100498348+JoseDOL@users.noreply.github.com>
2022-07-06 19:51:21 -0600

    segundo

--- gif-expert-app/src/GifExpertApp.jsx
+++ gif-expert-app/src/GifExpertApp.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+// zSJoHEX6YctsGwsW8G4wqFXVDIp6W9BH api 
+import {React, useState} from 'react';
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
 
 export const GifExpertApp = () => {
-  return (
+    const [ categories, setCategories ]= useState(['One Punch', 'Dragon Ball'])
+    
+    const onAddCategory = (newCategory) =>{
+
+      if(categories.includes(newCategory))return;
+
+      setCategories ([newCategory, ...categories ])
+      //setCategories (cat =>[...cat, 'Valorant' ]) 
+    }
+    return (
     <>
     <h1>GifExpertApp</h1>
-    
+
+    <AddCategory 
+      onNewCategory = { (value)=> onAddCategory(value)}
+    />
+      {
+        categories.map((category) =>(
+          <GifGrid 
+          key={category} 
+          category={category} 
+          />
+        ))
+      
+      
+      }
     </>
   )
 }
