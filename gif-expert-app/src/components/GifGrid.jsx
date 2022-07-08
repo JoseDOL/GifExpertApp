175edde16175afa1a4b97bef9d679b9dae11c238
JoseDOL <100498348+JoseDOL@users.noreply.github.com>
2022-07-07 19:40:17 -0600

    tercer

--- gif-expert-app/src/components/GifGrid.jsx
+++ gif-expert-app/src/components/GifGrid.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import { getGifs } from '../helpers/getGifs';
+import {GifItem} from '../components/GifItem';
+
 
 export const GifGrid = ({category}) => {
+  const [images, setImages] = useState([]);
+
+  const getImages = async () =>{
+    const newImage = await getGifs(category);
+    setImages(newImage);
+  }
+useEffect(() =>{
+  getImages();
+}, [ ]);
+  
   return (
     <>
         <h3>{category}</h3>
+        <div className='card-grid'>
+          {
+            images.map((image)=>(
+              <GifItem 
+              key={image.id}
+              {...image}
+              />
+            ))
+          }
+        </div>
     </>
   )
 }
